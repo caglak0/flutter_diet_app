2fc728a78df193d57b09e52f4e7ad15a11c2e2ed
Muhammet Yurt <muhammetyrt18@gmail.com>
2024-04-23 15:53:03 +0300

    Tema -muhammet

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_diet_app/pages/home_screen.dart';
 import 'package:flutter_diet_app/theme/light_tema.dart';
+import 'package:provider/provider.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeData _currentTheme = ThemeData.light();
+
+  ThemeData get currentTheme => _currentTheme;
+
+  void toggleTheme() {
+    _currentTheme = _currentTheme == ThemeData.light() ? ThemeData.dark() : ThemeData.light();
+    notifyListeners();
+  }
+}
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,12 +28,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
-        theme: LighTema().theme,
-        darkTheme: ThemeData.dark(),
-        themeMode: ThemeMode.system,
+        theme: themeProvider._currentTheme,
         home: const NavbarTheme());
   }
 }
